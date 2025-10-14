ddb3cd678e8afd23fd1e13b8ee0d3435821cd655
Mateus Silva <187775972+msilva-dev@users.noreply.github.com>
2025-10-14 16:27:26 -0300

    refactor(ContaBancaria): Um simples ajuste no REGEX_EMAIL

--- src/main/java/br/com/mateussilva/financeiro/domain/util/ValidadorUtil.java
+++ src/main/java/br/com/mateussilva/financeiro/domain/util/ValidadorUtil.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class ValidadorUtil {
 
-    private static final String REGEX_EMAIL = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
+    private static final String REGEX_EMAIL = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
     private static final Pattern PADRAO_EMAIL = Pattern.compile(REGEX_EMAIL);
 
     public static boolean isEmailValido(String email) {
