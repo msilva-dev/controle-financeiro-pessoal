8edec52a8143925afdf19b9ea29f8f75030735df
Mateus Silva <187775972+msilva-dev@users.noreply.github.com>
2025-10-14 16:23:08 -0300

    feat(domain): Implementa a entidade imutável Transacao e o seu Enum

--- src/main/java/br/com/mateussilva/financeiro/domain/model/transacao/Transacao.java
+++ src/main/java/br/com/mateussilva/financeiro/domain/model/transacao/Transacao.java
@@ -1,7 +1,5 @@
 package br.com.mateussilva.financeiro.domain.model.transacao;
 
-import br.com.mateussilva.financeiro.domain.model.conta.Conta;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -11,10 +9,29 @@ public record Transacao(UUID id,
                         BigDecimal valor,
                         TipoTransacao tipo,
                         LocalDateTime dataHora,
-                        String destino) {
+                        String beneficiario) {
 
-    Transacao(String descricao, BigDecimal valor, TipoTransacao tipo, String destino) {
-        this(UUID.randomUUID(), descricao, valor, tipo, LocalDateTime.now(), destino);
+    public Transacao {
+        if (descricao == null || descricao.trim().isBlank()) {
+            throw new IllegalArgumentException(MSG_DESCRICAO_INVALIDA);
+        }
+        if (valor == null || valor.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException(MSG_VALOR_INVALIDO);
+        }
+        if (tipo == null) {
+            throw new IllegalArgumentException(MSG_TIPO_TRANSACAO_INVALIDA);
+        }
+        if (beneficiario == null || beneficiario.trim().isBlank()) {
+            throw new IllegalArgumentException(MSG_BENEFICIARIO_INVALIDO);
+        }
     }
 
+    public Transacao(String descricao, BigDecimal valor, TipoTransacao tipo, String beneficiario) {
+        this(UUID.randomUUID(), descricao, valor, tipo, LocalDateTime.now(), beneficiario);
+    }
+
+    public static final String MSG_DESCRICAO_INVALIDA = "ERRO: Uma descrição deve ser informada.";
+    public static final String MSG_TIPO_TRANSACAO_INVALIDA = "ERRO: Um tipo de transação deve ser informada.";
+    public static final String MSG_BENEFICIARIO_INVALIDO = "ERRO: O beneficiário da transação deve ser informado.";
+    public static final String MSG_VALOR_INVALIDO = "ERRO: O valor deve ser maior que zero.";
 }
